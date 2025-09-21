035bcaf02943004fa176e3053eb10e23c89aecdc
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-09-21 00:30:15 -0400

    remove test code

--- sketches/app/lib-03-debug.ino
+++
@@ -1,8 +0,0 @@
-void print_touch() {
-  // char print_buffer[30];
-  // for (int i=0; i < N_TOUCH; i++) {
-  //   sprintf(print_buffer, "%4d ", touch_values[i]);
-  //   Serial.print(print_buffer);
-  // }
-  // Serial.println("");
-}
