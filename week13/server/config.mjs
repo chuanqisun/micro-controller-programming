446747809441cbba3c1806b8f57594f28ecd93ab
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-11-29 20:04:14 -0500

    remove dead code

--- week13/server/config.mjs
+++ week13/server/config.mjs
@@ -2,7 +2,6 @@ export const SAMPLE_RATE = 24000;
 export const CHANNELS = 1;
 export const BITS_PER_SAMPLE = 16;
 export const LAPTOP_UDP_RX_PORT = 8888;
-export const ESP32_UDP_RX_PORT = 8889;
 export const HTTP_PORT = 3000;
 export const PACKET_SIZE = 1024;
 export const SILENCE_TIMEOUT_MS = 1000;
