218812b8d43489423e12cf03ab63dc856c5f93df
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-12-02 19:39:44 -0500

    disable gemini faeures

--- week13/web/src/features/simulation.ts
+++ week13/web/src/features/simulation.ts
@@ -6,10 +6,10 @@ export function initSimulationUI() {
   connectBtnSim.addEventListener("click", async () => {
     if (connectBtnSim.textContent === "Connect") {
       fetch("http://localhost:3000/api/ai/connect", { method: "POST" });
-      fetch("http://localhost:3000/api/gemini/connect", { method: "POST" });
+      // fetch("http://localhost:3000/api/gemini/connect", { method: "POST" });
     } else {
       fetch("http://localhost:3000/api/ai/disconnect", { method: "POST" });
-      fetch("http://localhost:3000/api/gemini/disconnect", { method: "POST" });
+      // fetch("http://localhost:3000/api/gemini/disconnect", { method: "POST" });
     }
   });
 }
